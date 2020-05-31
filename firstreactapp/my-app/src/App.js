cc9b5e3331967a0a4bdaf58de3f9628a700136e5
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-05-31 23:18:40 +0500

    React practice

--- firstreactapp/my-app/src/App.js
+++ firstreactapp/my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Hello from "./sayHello";
+import Tweet from './Tweet';
+
 function App(){
 
   return(
-    <div> 
-      <h1> This is the app component</h1>
-<Hello />
+    <div className="app"> 
+      <Tweet name="Sonya" message="This is a random tweet" likes="Likes: 770" />
+      <Tweet name="Danya" message="I am the true king" likes="Likes: 990" />
+      <Tweet name="Ksenia" message="700k my dudes"likes="Likes: 110" />
+      <Tweet name="Alex" message="My course is available" likes="Likes: 508"/>
     </div>
   )
 }
