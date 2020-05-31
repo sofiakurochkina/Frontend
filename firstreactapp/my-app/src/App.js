106d78888cfc87ba64ff55391ace848cb3351d75
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-05-31 22:53:13 +0500

    React practice

---
+++ firstreactapp/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from "./sayHello";
+function App(){
+
+  return(
+    <div> 
+      <h1> This is the app component</h1>
+<Hello />
+    </div>
+  )
+}
+export default App;
